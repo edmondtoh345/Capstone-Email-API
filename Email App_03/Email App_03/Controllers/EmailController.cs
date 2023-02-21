e6fca6286be287295c8181214b807f3d7f8b89a6
edmond.toh <edmond.toh@temus.com>
2023-02-21 17:05:47 +0800

    first commit

---
+++ Email App_03/Email App_03/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using Email_App_03.Service;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace Email_App_03.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+            private readonly IEmailService emailService;
+
+            public EmailController(IEmailService emailService)
+            {
+                this.emailService = emailService;
+            }
+
+            [HttpPost]
+            public IActionResult SendMail(string to, string subject, string body)
+            {
+                emailService.SendMail(to, subject, body);
+                return Ok("Email sent successfully");
+            }
+        }
+    }
+
