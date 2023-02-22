a62114a87f1c8e0b19a228718208de079d6fef6a
edmond.toh <edmond.toh@temus.com>
2023-02-22 18:44:21 +0800

    update01

--- Email App_03/Email App_03/Controllers/EmailController.cs
+++ Email App_03/Email App_03/Controllers/EmailController.cs
@@ -1,28 +1,65 @@
 ﻿using Email_App_03.Service;
-using MailKit.Net.Smtp;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using MimeKit;
+using System.Net.Mail;
 
-namespace Email_App_03.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class EmailController : ControllerBase
+    private readonly IEmailService emailService;
+
+    public EmailController(IEmailService emailService)
     {
-            private readonly IEmailService emailService;
-
-            public EmailController(IEmailService emailService)
-            {
-                this.emailService = emailService;
-            }
-
-            [HttpPost]
-            public IActionResult SendMail(string to, string subject, string body)
-            {
-                emailService.SendMail(to, subject, body);
-                return Ok("Email sent successfully");
-            }
-        }
+        this.emailService = emailService;
     }
 
+    [HttpPost]
+    public IActionResult SendMail(string to, string subject, string body)
+    {
+        emailService.SendMail(to, subject, body);
+        return Ok("Email sent successfully");
+    }
+
+    //[HttpPost("forgot-password")]
+    //public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordRequest request)
+    //{
+    //    // Check if the user exists in the database
+    //    var user = await _userService.GetUserByEmail(request.Email);
+    //    if (user == null)
+    //    {
+    //        return BadRequest("User not found");
+    //    }
+
+    //    // Generate a unique token
+    //    var token = Guid.NewGuid().ToString();
+
+    //    // Store the token in the database along with the user's email and an expiration time
+    //    await _userService.CreatePasswordResetToken(user.Id, token);
+
+    //    // Generate the email body using a view template
+    //    var model = new ResetPasswordViewModel
+    //    {
+    //        ResetLink = $"{_appSettings.ResetPasswordBaseUrl}?token={token}"
+    //    };
+    //    var body = await _viewRenderer.RenderViewAsync("ResetPasswordEmail", model);
+
+    //    // Send the email using the email service
+    //    try
+    //    {
+    //        var message = new EmailMessage
+    //        {
+    //            To = user.Email,
+    //            Subject = "Reset your password",
+    //            Body = body
+    //        };
+    //        await _emailService.SendEmailAsync(message);
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        // Handle the exception and return an appropriate error message
+    //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+    //    }
+
+    //    return Ok();
+    //}
+}
